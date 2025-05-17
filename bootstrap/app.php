4a1fb62e2507f7d3dac688eca3150599a272a2c0
Lex <74861308+GenghisChan1@users.noreply.github.com>
2025-05-17 12:00:32 +0800

    configure cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,19 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // Add global middleware
+        $middleware->append([
+            \Illuminate\Http\Middleware\HandleCors::class, // Laravel's built-in CORS
+        ]);
+        
+        // Or if using custom CORS middleware:
+        // $middleware->append(\App\Http\Middleware\CorsMiddleware::class);
+        
+        // API middleware group (applied to routes in routes/api.php)
+        $middleware->group('api', [
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test-cors', function() {
+    return response()->json([
+        'cors_config' => config('cors'),
+        'message' => 'CORS test from web route'
+    ]);
+});
